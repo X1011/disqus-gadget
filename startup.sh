7aee87779eb7510b32097586d0f21aaa238bf7ef
X1011 <xx1011@gmail.com>
2014-12-09 07:30:35 +0000

    BrowserSync

--- startup.sh
+++ startup.sh
@@ -1,2 +1,6 @@
 ungit --port=9501 &
-versal preview
+
+#for some reason, when using https, i get a gateway timeout when trying to load the page
+browser-sync start --server --files '*.html,*.js,*.css' --port 1024 &
+
+#versal preview &
