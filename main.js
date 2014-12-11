46186b6fcd9afa5538a5c457bcc411ae5fc0982e
X1011 <xx1011@gmail.com>
2014-12-11 08:56:04 +0000

    move embed code to main.js
    + clean up

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 import player from 'versal-player-api';
+
+var disqus_shortname = 'x1011-test';
+
+var dsq = document.createElement('script');
+dsq.async = true;
+dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+document.getElementsByTagName('head')[0].appendChild(dsq);
